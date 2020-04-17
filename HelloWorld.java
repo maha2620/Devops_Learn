e1cd22faa94d25907a5f647047306a9c870ffa71
maha2620 <maheedevops@gmail.com>
2020-04-17 19:47:20 +0530

    committed again

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
-class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-    }  
-} 
+/** 
+ * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+  }
+}
